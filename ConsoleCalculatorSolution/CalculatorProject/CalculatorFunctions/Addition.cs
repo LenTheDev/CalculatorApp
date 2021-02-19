d727a1e161a5b172bcc7992069a8296c67fbd196
LenTheDev <lhl7@njit.edu>
2021-02-19 09:11:32 -0500

    added calculator functions for other operations

---
+++ ConsoleCalculatorSolution/CalculatorProject/CalculatorFunctions/Addition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorProject.CalculatorFunctions
+{
+    public class Addition
+    {
+        public static double Sum(double a, double b)
+        {
+            double _c;
+            _c = a + b;
+            return _c;      
+        }
+    }
+
+    public class Subtract
+    {
+        public static double Subtraction(double a, double b)
+        {
+            double _c;
+            _c = b - a;
+            return 
+        }
+      
+    }
+
+    
+    public class Multiply
+    {
+        public static double Multiplication double a, double b)
+        {
+            double _c;
+            _c = a * b;
+            return _c;
+        }
+      
+    }
+
+
+    public class Divide
+    {
+        public static double Division(double a, double b, MidpointRounding)
+        {
+            double _c;
+            _c = MidpointRounding(b / a, 9); /* Second Parameter Rounds by decimal places)*/
+            return _c
+        }
+      
+    }
+}
