d101fb4e242a80e659b3bb1c617f752e631301d4
LenTheDev <lhl7@njit.edu>
2021-02-26 08:47:50 -0500

    "Feature: Created an Abstract Factory"

---
+++ Interfaces/InterfaceCalc.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperationInterface : Operations
+{
+
+    interface Operation
+    {
+
+        private double SumList;
+        private double SubtractionList;
+    }
+
+}
